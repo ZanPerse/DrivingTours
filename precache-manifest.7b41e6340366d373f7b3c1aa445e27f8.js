22d14974d0e0d23bfd37f139803cef26d55cae25
ZanPerse <zanperse1@gmail.com>
2019-11-13 10:15:42 +0100

    Updates

---
+++ precache-manifest.7b41e6340366d373f7b3c1aa445e27f8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d2095b288217b537bce198e64a5d13",
+    "url": "/DrivingTours/index.html"
+  },
+  {
+    "revision": "c9dc8d7b24f8740718a6",
+    "url": "/DrivingTours/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "54c6eb6521a5f57d340e",
+    "url": "/DrivingTours/static/css/main.e25a3d61.chunk.css"
+  },
+  {
+    "revision": "c9dc8d7b24f8740718a6",
+    "url": "/DrivingTours/static/js/2.aa0d9d3b.chunk.js"
+  },
+  {
+    "revision": "54c6eb6521a5f57d340e",
+    "url": "/DrivingTours/static/js/main.e20922c9.chunk.js"
+  },
+  {
+    "revision": "92cd6f436892badaf2f2",
+    "url": "/DrivingTours/static/js/runtime-main.ccde2431.js"
+  },
+  {
+    "revision": "27b447d84ac4d72039ba3e6e48336924",
+    "url": "/DrivingTours/static/media/dayone.27b447d8.jpg"
+  },
+  {
+    "revision": "341ec62c7659eee57eff12babb596a5b",
+    "url": "/DrivingTours/static/media/daythree.341ec62c.jpg"
+  },
+  {
+    "revision": "c515781b830a16ce3f62e88c655b6213",
+    "url": "/DrivingTours/static/media/daytwo.c515781b.jpg"
+  }
+]);
